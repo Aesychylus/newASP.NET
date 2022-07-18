49283dbafacb781edc79108ef7d120f06dba8acb
Aesychylus <106658863+Aesychylus@users.noreply.github.com>
2022-07-18 16:45:59 -0400

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,30 @@
+﻿function validate() {
+
+    var oneNumber = document.getElementById("firstBox").value;
+    var secondNumber = document.getElementById("secondBox").value;
+    var listItem = document.getElementById("DropDownList").value;
+
+    if (oneNumber == "") {
+        alert("Enter value of x");
+        return false;
+    } else if (secondNumber == "") {
+        alert("Enter value of y");
+        return false;
+    } else if (listItem == "") {
+        alert("Please select an operator");
+        return false;
+    }
+    else {
+        alert("Validation is successful");
+        return true;
+    }
+}
+
+function Number(evt) {
+    var charCode = (evt.which) ? evt.which : event.keyCode;
+    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+
+        alert("Please enter only numeric digits");
+        return false;
+    }
+}
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Calculator
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void Calculation(object sender, EventArgs e)
+        {
+            var firstNumber =Convert.ToDecimal(this.firstBox.Text);
+            var secondNumber = Convert.ToDecimal(this.secondBox.Text);
+            var opt = this.DropDownList.SelectedValue;
+            var rslt=0;
+
+            if (opt == "+")
+            {
+                rslt = Convert.ToInt32(firstNumber + secondNumber);
+            }
+            else if (opt == "-")
+            {
+                rslt = Convert.ToInt32(firstNumber - secondNumber);
+            }
+            else if (opt == "*")
+            {
+                rslt = Convert.ToInt32(firstNumber * secondNumber);
+            }
+            else if (opt == "/")
+            {
+                rslt = Convert.ToInt32(firstNumber / secondNumber);
+            }
+            resultLabel.Text = rslt.ToString();        
+        }   
+
+    }
+}
